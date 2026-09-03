aa4fd7a6d4377ccbaa0ca6c1f5778a54567dd6ae
samir mangroliya <sbmangroliya2012@gmail.com>
2026-09-03 15:17:41 +0530

    Initialize E-Commerce Modular App project structure
    - Set up root project and `:app` module using Gradle Kotlin DSL.

---
+++ app/src/main/java/com/samir/ecommerceapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.samir.ecommerceapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.samir.ecommerceapp.ui.theme.ECommerceModularAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ECommerceModularAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ECommerceModularAppTheme {
+        Greeting("Android")
+    }
+}
