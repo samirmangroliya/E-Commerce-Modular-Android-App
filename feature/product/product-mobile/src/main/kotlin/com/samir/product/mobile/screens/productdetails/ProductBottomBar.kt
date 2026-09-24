16fda3cc1fd66e6af68fd4f8c40a56fe04359ccc
Samir Mangroliya <samirmangroliya@users.noreply.github.com>
2026-09-24 15:51:17 +0530

    Update Android CI workflow and build target for mobile app (#4)
    * Update Android CI workflow and build target for mobile app
    
    * Update Gradle wrapper and adjust ProductBottomBar padding
    
    * Update Gradle wrapper distribution checksum
    * Increase vertical padding in `ProductBottomBar` layout

--- feature/product/product-mobile/src/main/kotlin/com/samir/product/mobile/screens/productdetails/ProductBottomBar.kt
+++ feature/product/product-mobile/src/main/kotlin/com/samir/product/mobile/screens/productdetails/ProductBottomBar.kt
@@ -36,7 +36,7 @@ fun ProductBottomBar(
                 .fillMaxWidth()
                 .padding(
                     horizontal = 16.dp,
-                    vertical = 4.dp
+                    vertical = 8.dp
                 ),
             horizontalArrangement = Arrangement.spacedBy(12.dp)
         ) {
